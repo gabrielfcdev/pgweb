1f80ccb59042b9f5ddecc697025ae1cfae80928e
Gabriel Calixto <gfcdev@outlook.com.br>
2024-07-25 09:53:03 -0300

    atualização

---
+++ script.js
@@ -0,0 +1,10 @@
+/*const para = document.querySelector("p");
+
+para.addEventListener("click", atualizarNome);
+
+
+function atualizarNome() {
+  var nome = prompt("Insira um novo nome");
+  para.textContent = "Jogador 1: " + nome;
+}*/
+
